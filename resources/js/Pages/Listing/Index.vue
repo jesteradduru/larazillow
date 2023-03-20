ab3de2a79fc1aaf40544d01eb52f02eab5f355fa
Jester Adduru <jesteradduru@gmail.com>
2023-03-20 11:45:52 +0800

    add edit form, delete functionality, ziggy to use route names

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,8 +1,18 @@
 <template>
     <div v-for="listing in listings" :key="listing.id">
-        <Link :href="`/listing/${listing.id}`">
+        <Link :href="route('listing.show', {listing: listing.id})">
             <ListingAddress :listing="listing"/>
         </Link>
+        <div>
+            <Link :href="route('listing.update', {listing: listing.id})">
+                Edit
+            </Link>
+        </div>
+        <div>
+            <Link :href="route('listing.destroy', {listing: listing.id})" method="DELETE" as="button">
+                Delete
+            </Link>
+        </div>
     </div>
 </template>
 <script setup>
