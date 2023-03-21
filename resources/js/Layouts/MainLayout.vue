9caa0983bb0073f5f940f5d8f25769114cfc2af3
Jester Adduru <jesteradduru@gmail.com>
2023-03-21 17:02:37 +0800

    added login and logout, apply protected routes using middleware

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,6 +3,7 @@ import { Link, usePage } from '@inertiajs/vue3'
 import { computed } from 'vue'
 
 const flashSuccess = computed(() => usePage().props.flash.success)
+const user = computed(() => usePage().props.user)
 </script>
 
 <template>
@@ -15,14 +16,23 @@ const flashSuccess = computed(() => usePage().props.flash.success)
         <div class="text-xl font-bold text-indigo-600 dark:text-indigo-400">
           <Link :href="route('listing.index')">LaraZillow</Link>
         </div>
-        <div class="btn-primary">
-          <Link :href="route('listing.create')">+ New Listing</Link>
+        <div v-if="user" class="flex gap-3 items-center">
+          <div>{{ user.name }}</div>
+          
+          <div class="btn-primary">
+            <Link :href="route('listing.create')">+ New Listing</Link>
+          </div>
+
+          <Link :href="route('logout')" method="DELETE" as="button">Logout</Link>
+        </div>
+        <div v-else>
+          <Link class="btn-primary" :href="route('login')">Sign-in</Link>
         </div>
       </nav>
     </div>
   </header>
     
-  <main class="container mx-auto p-3">
+  <main class="container mx-auto p-3 w-full">
     <div v-if="flashSuccess" class="p-2 border rounded-md border-green-500 bg-green-100 dark:bg-green-900 dark:border-green-700 shadow-md mb-2">
       {{ flashSuccess }}
     </div>
