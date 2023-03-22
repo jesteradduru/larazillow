83dbabc5a8e1d074c27f1f2262efffa4adfd55ee
Jester Adduru <jesteradduru@gmail.com>
2023-03-22 16:54:16 +0800

    added pagination using paginate() and orderByDesc in ListingController.index

---
+++ resources/js/Components/UI/Pagination.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+defineProps({
+  links: Array,
+})
+</script>
+
+<template>
+  <div class="flex gap-4 my-8">
+    <div v-for="(link, index) in links" :key="index">
+      <Link :href="link.url" :class="link.active ? 'bg-indigo-800 px-2 py-1 rounded-md' : 'px-2 py-1 rounded-md hover:text-indigo-300 '" v-html="link.label" />
+    </div>
+  </div>
+</template>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,12 +1,16 @@
 <script setup>
 import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
+import Pagination from '@/Components/UI/Pagination.vue'
 defineProps({
-  listings: Array,
+  listings: Object,
 })
 </script>
 
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-    <Listing v-for="listing in listings" :key="listing.id" :listing="listing" />
+    <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
+  </div>
+  <div class="flex justify-center mx-auto">
+    <Pagination :links="listings.links" />
   </div>
 </template>
