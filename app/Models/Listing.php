4734cf54677ade8c59fd1d8ee22605c03be05fe5
Jester Adduru <jesteradduru@gmail.com>
2023-03-27 11:05:30 +0800

    added File Data Model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Listing extends Model
@@ -16,9 +17,14 @@ class Listing extends Model
     
     protected $sortable = ['price', 'created_at'];
 
-    public function owner() : HasOne
+    public function owner() : BelongsTo
     {
-        return $this->hasOne(User::class, 'by_user_id');
+        return $this->belongsTo(User::class, 'by_user_id');
+    }
+
+    public function images() : HasMany
+    {
+        return $this->hasMany(Listing::class);
     }
 
     public function scopeFilter(Builder $query, array $filters) : Builder
