721795886e428250b16ea37a6a03c2309fc73f01
Jester Adduru <jesteradduru@gmail.com>
2023-03-31 14:21:14 +0800

    145 - Seeing Which Offer is Sold

--- resources/js/Pages/Realtor/Show/Components/Offer.vue
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -1,6 +1,9 @@
 <template>
   <Box>
-    <template #header>Offer #{{ offer.id }}</template>
+    <template #header>
+      Offer #{{ offer.id }} 
+      <span v-if="offer.accepted_at" class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 p-1 rounded-md uppercase ml-1">accepted</span>
+    </template>
   
     <section class="flex items-center justify-between">
       <div>
@@ -20,6 +23,7 @@
       </div>
       <div>
         <Link
+          v-if="notSold"
           class="btn-outline text-xs font-medium" 
           as="button" :href="route('realtor.offer.accept', {offer: offer.id})" method="put"
         >
@@ -44,5 +48,9 @@ const difference = computed(
 )
 const madeOn = computed(
   () => new Date(props.offer.created_at).toDateString(),
+)
+
+const notSold = computed(
+  () => !props.offer.accepted_at && !props.offer.rejected_at,
 )
 </script>
