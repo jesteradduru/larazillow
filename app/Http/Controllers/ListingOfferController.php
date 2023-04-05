54b39ac3ce49ccd0fe5ac14a24207e7324a1579c
Jester Adduru <jesteradduru@gmail.com>
2023-04-05 11:32:58 +0800

    152 - Sending Notification When Offer is Made

--- app/Http/Controllers/ListingOfferController.php
+++ app/Http/Controllers/ListingOfferController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use App\Models\Offer;
+use App\Notifications\OfferMade;
 use Illuminate\Http\Request;
 
 class ListingOfferController extends Controller
@@ -16,13 +17,15 @@ class ListingOfferController extends Controller
        
         $this->authorize('offer', $listing);
 
-        $listing->offers()->save(
+        $offer = $listing->offers()->save(
             Offer::make(
                 $request->validate([
                     'amount' => 'integer|required|min:1|max:2000000000'
                 ])
             )->bidder()->associate($request->user())
         );
+
+        $listing->owner->notify(new OfferMade($offer));
 
         return redirect()->back()->with('success', 'Offer made successfully');
     }
